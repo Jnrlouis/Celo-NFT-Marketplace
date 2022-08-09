f40f92c49497a75478b75a27e8810c3755de52f3
JnrLouis <chibuezelouis@gmail.com>
2022-08-09 02:16:41 +0100

    marketplace

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/jnrlouis/celo-nft-marketplace";
+
+export const MARKETPLACE_ADDRESS = "0xa1413e9799305AdF23e764D26eE958869f2aE6FF";
